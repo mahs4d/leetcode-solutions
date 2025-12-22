6a9db7cd1024f3e9271ff97d17e5d3e8c785be6a
Mahdi Sadeghi <mahdi74sadeghi@gmail.com>
2025-12-22 23:27:40 +0100

    Add solution for problem 1211.

---
+++ problem-1211/README.md
@@ -0,0 +1,3 @@
+# 1211. Queries Quality and Percentage
+
+[link](https://leetcode.com/problems/queries-quality-and-percentage/)
---
+++ problem-1211/solution.sql
@@ -0,0 +1,6 @@
+select
+    query_name,
+    round(avg(rating::decimal / position), 2) as quality,
+    round(((sum(case when rating < 3 then 1 else 0 end)::decimal / count(*)) * 100), 2) as poor_query_percentage
+from queries
+group by query_name;
